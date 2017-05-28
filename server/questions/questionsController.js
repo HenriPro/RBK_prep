221ef02aa5699ce2b48ce47c06d4882b34e12770
Fatima Hammami <en.f.h@live.com>
2017-05-28 15:13:14 +0300

    feat add model

---
+++ server/questions/questionsController.js
@@ -0,0 +1,13 @@
+var question=require('./questionsModel.js');
+
+module.exports={
+	addQuestion: function ( req, res ) {
+
+	},
+	removeQuestion: function ( req, res ) {
+
+	},
+	editQuestion: function ( req, res ) {
+		
+	}
+}
---
+++ server/questions/questionsModel.js
@@ -0,0 +1,17 @@
+var mongoose=require('mongoose');
+
+var QuestionsSchema= new mongoose.Schema({
+	type: {
+		type: String,
+		required: true
+	},
+	question: {
+		type: String,
+		required: true
+	},
+	example : String,
+	image : String,
+	lectureID: {type: mongoose.Schema.Types.ObjectId, ref: 'questions'}
+})
+
+module.exports=mongoose.model('questions',QuestionsSchema);
