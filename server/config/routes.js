ca055a25a5f39b866ec2b1a995b515a755c21d70
Fatima Hammami <en.f.h@live.com>
2017-06-01 14:04:38 +0300

    feat finish questions routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,14 +3,34 @@ var questionsController= require('../questions/questionsController.js');
 var lecturesController=require('../lectures/lecturesController.js');
 
 module.exports=function (app, express){
+
+	////////////////////////////////////////////////////////
+	//													///
+	//				Users Routes						///	
+	//													///
+	///////////////////////////////////////////////////////
+
 	app.post('/api/users/signin',usersController.signin);
 	app.post('/api/users/signup',usersController.signup);
 
+	////////////////////////////////////////////////////////
+	//													///
+	//				Questions Routes					///	
+	//													///
+	///////////////////////////////////////////////////////
+	
 	app.get('/api/questions/getQuestion/:id', questionsController.getQuestion);
-	app.get('/api/questions/getAllQuestions',questionsController.getAllQuestions);
+	app.get('/api/questions/getAllLectureQuestions/:lectureID', questionsController.getAllLectureQuestions);
 	app.post('/api/questions/addQuestion', questionsController.addQuestion);
 	app.put('/api/questions/editQuestion', questionsController.editQuestion);
+	app.delete('/api/questions/removeQuestion/:id', questionsController.removeQuestion);
 
+	////////////////////////////////////////////////////////
+	//													///
+	//				Lectures Routes						///	
+	//													///
+	///////////////////////////////////////////////////////
+	
 	app.get('/api/lectures/getLecture',lecturesController.getLecture);
 	app.get('/api/lectures/getAllLectures',lecturesController.getAllLectures);
 	app.post('/api/lectures/addLecture', lecturesController.addLecture);
