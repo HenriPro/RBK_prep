274aaa0b30df2acf4d8da40fba8d09f8b66eeb2b
Fatima <en.fh90@gmail.com>
2017-06-07 17:11:18 +0300

    feat creat auth functions

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,6 +1,17 @@
 angular.module('prep.signin',[])
 
-.controller('SigninController',function($scope){
-	$scope.username= {};
-	console.log("hey there")
+.controller('SigninController',function($scope, Auth){
+	$scope.user = {};
+
+	$scope.signin = function (){
+		Auth.signin($scope.user)
+		.then(function(resp){
+			console.log(resp)
+			if(!resp.token){
+				console.log("there were no response");
+			}else{
+				console.log(resp.token)
+			}
+		})
+	}
 })
