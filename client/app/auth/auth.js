3d8472458ac82f2b53881f321ec018b68722d7f8
Fatima <en.fh90@gmail.com>
2017-07-12 21:49:38 +0300

    fix styling

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -3,7 +3,6 @@ angular.module('prep.signin',[])
 .controller('SigninController',function($scope, Auth, $location, $window, $rootScope){
 	$scope.user = {};
 	$rootScope.authenticated= false;
-	console.log("auth",$rootScope.authenticated)
 
 	$scope.signin = function () {
 		Auth.signin($scope.user)
@@ -13,7 +12,7 @@ angular.module('prep.signin',[])
 				alert('Wrong Username or Password');
 			}else{
 				$window.localStorage.setItem('rbk.prep', resp.token);
-				$location.path('/Array-Iteration-with-while-&-for');
+				$location.path('/calendar');
 			}
 		})
 	}
